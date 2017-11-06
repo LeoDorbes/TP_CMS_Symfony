d4f76d9bd41c588769af396e755f153f0f7c14d9
Cyrille Benoit <cyrille.benoit@ymail.com>
2017-11-06 09:16:10 +0000

    Routes done.

--- src/RouteBundle/Controller/DefaultController.php
+++ src/RouteBundle/Controller/DefaultController.php
@@ -10,35 +10,25 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="front_home")
-     * @Route("/", name="front_category")
-     * @Route("/", name="front_article")
-     * @Route("/", name="front_search")
-     * @Route("/", name="front_login")
+     * @Route("/category/{slug}", name="front_category")
+     * @Route("/category/{category}/article/{slug}", name="front_article")
+     * @Route("/search", name="front_search")
+     * @Route("/login", name="front_login")
      */
-    public function frontAction()
+    public function frontAction($slug, $category)
     {
-        // @todo Make routes
-        //       Set front_category route to be /category/[slug]
-        //       Set front_article  route to be /category/[category]/article/[slug]
-        //       Set front_search   route to be /search
-        //       Set front_login    route to be /login
-
         return $this->render("::front.html.twig");
     }
 
     /**
      * @Route("/admin", name="admin_home")
-     * @Route("/", name="admin_settings")
-     * @Route("/", name="admin_modules")
-     * @Route("/", name="admin_content")
+     * @Route("/admin/settings/{options}", name="admin_settings")
+     * @Route("/admin/modules/{options}", name="admin_modules")
+     * @Route("/admin/content/{options}", name="admin_content")
+     * @return
      */
-    public function adminAction()
+    public function adminAction($options = null)
     {
-        // @todo Make routes
-        //       Set admin_settings route to be /admin/settings/[options] with [options] set to null by default
-        //       Set admin_modules  route to be /admin/modules/[options]  with [options] set to null by default
-        //       Set admin_content  route to be /admin/content/[options]  with [options] set to null by default
-
         return $this->render("::admin.html.twig");
     }
 }
